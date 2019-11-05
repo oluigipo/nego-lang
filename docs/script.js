0f47d6b51dca92e5e0625183d6a914da58d6f51f
Luig1B <LuigiBenedetti2005@gmail.com>
2019-11-05 14:54:19 -0300

    Atualização

--- docs/script.js
+++ docs/script.js
@@ -10,6 +10,17 @@ function bold(str) {
     return `<strong>${str}</strong>`;
 }
 
+window.onload = function () {
+    let p = window.location.href.indexOf('#');
+    if (p === -1) return;
+
+    let code = window.location.href.slice(p + 1);
+    let content = documentation[code];
+    if (content === undefined) return;
+
+    showContent(code);
+}
+
 let current = "";
 function showContent(selected) {
     if (current === selected) return;
@@ -29,8 +40,8 @@ const p = "<p></p>";
 const documentation = {};
 // Home
 documentation["home_overview"] = `${bold("Nego")} is a simple and interpreted programming language. It's made by ${bold("tuas nega")}.`;
-documentation["home_variables"] = `${titleText("Variables")}`;
-documentation["home_ifelse"] = `${titleText("Conditionals Statements")}`;
+documentation["home_variables"] = `${titleText(`Variables`)}${p}Variables in Nego doesn't work very differently compared with other languages. The difference is that you just need to initialize it. See the example:${p}${codeBlock(`variable = value;`)}${p}This code will assign the ${bold("value")} to the ${bold("variable")}. Also, it'll do ${bold("variable")} live in the global scope.${p}Scopes are created when the program reads a '{' and are deleted when the program reads a '}'. The variable is created in that scope if it hasn't initialized before. See the example:${p}${codeBlock(`a = 0; // 'a' lives in the global scope.\nif (true) {\n\ta = 5; // Assign 5 to 'a'.\n}\nprint(a); // Output: 5\n\nif (true) {\n\tb = 10; // 'b' was initialized inside this block, so 'b' lives inside this scope.\n} // Here, 'b' will be deleted.\nprint(b); // Error. 'b' don't exists outside the 'if' scope.`)}${p}Variables are ${bold("dynamically typed")}. This means that you can assign any value of any type to any variable. See the example:${p}${codeBlock(`var = 10;\nprint(var);\nvar = "Hello";\nprint(var);`)}${p}The output is exactly what is expected. It will print "10" and "Hello" to the console.`;
+documentation["home_ifelse"] = `${titleText("Conditional Statements")}`;
 documentation["home_loops"] = `${titleText("Loops (while & loop statement)")}${p}There's 2 loops statement: ${bold("while")} and ${bold("loop")}.\n${p}The while statement is very simple. It'll run a block of code while a condition is true.${p}The syntax is also simple.\n${codeBlock(`while (expr) {\n\t// Code...\n}`)}Example:\n${codeBlock(`x = 10;\nwhile (x > 0) {\n\tprint(x * x);\n\tx = x - 1;\n}`)}\n\nThe loop statement is a ${bold("iterator loop")}, this means that it's perfect for iterators.${p}Syntax:\n${codeBlock(`loop (iterator : number) {\n\t// Code...\n}`)}\n${bold("iterator")} is a variable which will run from ${bold("0")} to ${bold("number")}.${p}Example:\n${codeBlock(`qnt = num(input());\nprint("A quantidade escolhida é " + str(qnt));\nloop (i : qnt) {\n\tprint(i);\n}`)}`;
 // Examples
 documentation["example_helloworld"] = codeBlock("print(\"Hello, World!\");");
